aadccb823b4507ecdd41640ca332b4ccd45ffe7e
papadarth <flea07@gmx.net>
2017-12-21 14:54:55 +0100

    running firstCNN
    plus additional prediction pics for Categorical Cross Entropy

--- firstCNN/first_cnn.py
+++ firstCNN/first_cnn.py
@@ -49,50 +49,48 @@ y_test  = a[800:]
 #plt.plot(a[:10,:].T);plt.show()
 
 class DenseLayers(nn.Module):
-    def __init__(self, input_size, hidden1_size, output_size):
-        super(DenseLayers, self).__init__()
-        self.linear1    = nn.Linear(input_size, hidden1_size)
-        self.batch_n    = nn.BatchNorm1d(hidden1_size)
-        self.relu       = nn.ReLU()
-        self.linear2    = nn.Linear(hidden1_size, output_size)
+	def __init__(self, input_size, hidden1_size, output_size):
+		super(DenseLayers, self).__init__()
+		self.linear1	= nn.Linear(input_size, hidden1_size)
+		self.batch_n	= nn.BatchNorm1d(hidden1_size)
+		self.relu	   = nn.ReLU()
+		self.linear2	= nn.Linear(hidden1_size, output_size)
 
-    def forward(self, x):
-        h   = self.relu(self.batch_n(self.linear1(x)))
-        out   = self.linear2(h)
-        return out
+	def forward(self, x):
+		h   = self.relu(self.batch_n(self.linear1(x)))
+		out   = self.linear2(h)
+		return out
 
 class firstCNN(nn.Module):
-    def __init__(self, input_size, filter_size1, filter_size2, output_size):
-        super(firstCNN, self).__init__()
-        self.layer1     = nn.Sequential(
-            nn.Conv1d(input_size, filter_size1, kernel_size=4),
-            nn.BatchNorm1d(filter_size1),
-            nn.ReLU(),
-            nn.MaxPool1d(2)
-            )
-        self.layer2     = nn.Sequential(
-            nn.Conv1d(filter_size1, filter_size2, kernel_size=4),
-            nn.BatchNorm1d(filter_size2),
-            nn.ReLU(),
-            nn.MaxPool1d(2)
-            )
-        self.fc = nn.Linear(filter_size2,4)
+	def __init__(self, input_size, filter_size1, filter_size2, output_size):
+		super(firstCNN, self).__init__()
+		self.layer1	 = nn.Sequential(
+			nn.Conv1d(input_size, filter_size1, kernel_size=4),
+			nn.BatchNorm1d(filter_size1),
+			nn.ReLU(),
+			nn.MaxPool1d(2)
+			)
+		self.layer2	 = nn.Sequential(
+			nn.Conv1d(filter_size1, filter_size2, kernel_size=4),
+			nn.BatchNorm1d(filter_size2),
+			nn.ReLU(),
+			nn.MaxPool1d(2)
+			)
+		self.fc = nn.Linear(filter_size2*13,4) ## how to predict the 13?
 
-    def forward(self, x):
-        x   = x.unsqueeze_(1)
-        print x.size()
-
-        out = self.layer1(x)
-        out = self.layer2(out)
-        print out.size()
-        out = self.fc(out)
-        return out
+	def forward(self, x):
+		x   = x.unsqueeze_(1)
+		out = self.layer1(x)
+		out = self.layer2(out)
+		out = out.view(x.size(0),-1)
+		out = self.fc(out)
+		return out
 
 input_size	= t
 output_size	= 4
-filter_size1    = 16
-filter_size2    = 32
-hidden1_size    = 128
+filter_size1	= 16
+filter_size2	= 32
+hidden1_size	= 128
 
 ### 2 Dense Layer Model
 #model = DenseLayers(input_size, hidden1_size, output_size)
@@ -101,25 +99,25 @@ model = firstCNN(1, filter_size1, filter_size2, output_size)
 
 criterion	= nn.CrossEntropyLoss()
 optimizer	= torch.optim.Adam(model.parameters(), lr=.001)
-rnd             = np.arange(0,800)
+rnd			 = np.arange(0,800)
 
-for i in range(10):
-    print i
-    for epoch in range(500):
-        rnd = np.random.permutation(rnd)
-        inputs = Variable(torch.from_numpy(x_train[rnd,:]))
-        targets = Variable(torch.from_numpy(y_train[rnd]))
+for i in range(1):
+	print i
+	for epoch in range(500):
+		rnd = np.random.permutation(rnd)
+		inputs = Variable(torch.from_numpy(x_train[rnd,:]))
+		targets = Variable(torch.from_numpy(y_train[rnd]))
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        print epoch,':',loss.data[0]
+		optimizer.zero_grad()
+		outputs = model(inputs)
+		loss = criterion(outputs, targets)
+		loss.backward()
+		optimizer.step()
+		print epoch,':',loss.data[0]
 
-    predicted = model(Variable(torch.from_numpy(x_test))).data.numpy()
-    plt.plot(predicted[::4,:].T,'b.')
-    plt.plot(predicted[1::4,:].T,'g+')
-    plt.plot(predicted[2::4,:].T,'rx')
-    plt.plot(predicted[3::4,:].T,'ko')
-    plt.show()
+	predicted = model(Variable(torch.from_numpy(x_test))).data.numpy()
+	plt.plot(predicted[::4,:].T,'b.')
+	plt.plot(predicted[1::4,:].T,'g+')
+	plt.plot(predicted[2::4,:].T,'rx')
+	plt.plot(predicted[3::4,:].T,'ko')
+	plt.show()
