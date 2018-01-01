189b5e2147ebd0f8ed90b49c4f6f453d38745c73
papadarth <flea07@gmx.net>
2018-01-01 21:59:18 +0100

    activation of linear layer shown; update of the .py

--- simple_dense/simple_dense.py
+++ simple_dense/simple_dense.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-pytorch test 1
+simple dense classifier
+	one layer simple dense 
+	prediting two classes for a simple time series model; one POI, no temporal dependencies
+	test result (MSEloss) and activation of the layer (weight) are plottet
 """
 
 __author__  = "Tobias Wagner"
+__version__ = '0.2'
 
 import numpy as np
 import pylab as plt
@@ -28,13 +32,13 @@ b[1::2] = 1
 #plt.plot(a[:10,:].T);plt.show()
 # Linear Regression Model
 class LinearRegression(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(LinearRegression, self).__init__()
-        self.linear = nn.Linear(input_size, output_size)  
-    
-    def forward(self, x):
-        out = self.linear(x)
-	return out
+	def __init__(self, input_size, output_size):
+		super(LinearRegression, self).__init__()
+		self.linear = nn.Linear(input_size, output_size)  
+	
+	def forward(self, x):
+		out = self.linear(x)
+		return out
 
 input_size	= t
 output_size	= 1
@@ -45,24 +49,25 @@ criterion	= nn.MSELoss()
 optimizer	= torch.optim.SGD(model.parameters(), lr=.001)
 
 for i in range(10):
-    print i
-    for epoch in range(100):
-        inputs = Variable(torch.from_numpy(a))
-        targets = Variable(torch.from_numpy(b))
+	print i
+	for epoch in range(100):
+		inputs = Variable(torch.from_numpy(a))
+		targets = Variable(torch.from_numpy(b))
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        print epoch,':',loss.data[0]
+		optimizer.zero_grad()
+		outputs = model(inputs)
+		loss = criterion(outputs, targets)
+		loss.backward()
+		optimizer.step()
+		print epoch,':',loss.data[0]
 
-    predicted = model(Variable(torch.from_numpy(a))).data.numpy()
-    plt.hist(predicted[::2])
-    plt.hist(predicted[1::2])
-    plt.show()
-#print predicted
-#plt.plot(a, y_train, 'ro', label='Original data')
-#plt.plot(a, predicted, label='Fitted line')
-#plt.legend()
-#plt.show()
+	predicted = model(Variable(torch.from_numpy(a))).data.numpy()
+	plt.figure(figsize=(8,4))
+	plt.subplot(121)
+	plt.hist(predicted[::2])
+	plt.hist(predicted[1::2])
+	plt.subplot(122)
+	plt.plot(model.linear.weight[0].data.numpy())
+	plt.vlines([5],0,.3,'r')
+	plt.savefig('prediction_e%02d'%(i))
+	plt.show()
