df19085f5b193199a208e837dcc5c6c971bebdb4
papadarth <flea07@gmx.net>
2018-01-02 10:33:48 +0100

    splitted train and test samples to be correct

--- simple_dense/simple_dense.py
+++ simple_dense/simple_dense.py
@@ -20,12 +20,14 @@ from torch.autograd import Variable
 
 
 ## generate data ##
-n   = 200
+n   = 400
 t   = 10
 a   = np.zeros((n,t),dtype=np.float32) 
 a[::1,5] = -1
 a[1::2,5] = 1
+c = np.copy(a)
 a   += np.random.randn(n,t)*.5
+c   += np.random.randn(n,t)*.5
 b   = np.zeros((n), dtype=np.float32)
 b[1::2] = 1
 
@@ -61,7 +63,7 @@ for i in range(10):
 		optimizer.step()
 		print epoch,':',loss.data[0]
 
-	predicted = model(Variable(torch.from_numpy(a))).data.numpy()
+	predicted = model(Variable(torch.from_numpy(c))).data.numpy()
 	plt.figure(figsize=(8,4))
 	plt.subplot(121)
 	plt.hist(predicted[::2])
