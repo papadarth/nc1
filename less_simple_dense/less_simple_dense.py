644eed211649e37fddff5417cdedfc50d55e49d3
papadarth <flea07@gmx.net>
2018-01-10 03:19:28 +0100

    version 0.2, using pytorchs Dataset and DataLoader

--- less_simple_dense/less_simple_dense.py
+++ less_simple_dense/less_simple_dense.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 """
-pytorch test 1
+learning pytorch
+	testing less simple / 2 layer network
+
+updates:
+	0.2 : the pytorch structures Dataset and DataLoader is used
 """
 
 __author__  = "Tobias Wagner"
+__version__ = 0.2
 
 import numpy as np
 import pylab as plt
@@ -14,37 +19,67 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
-
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
 ## generate data ##
-n   = 1000
-t   = 20
-a   = np.zeros((n,t),dtype=np.float32) 
-## a
-a[::4,5] = -1
-a[::4,15] = -1
-## b
-a[1::4,5] = 1
-a[1::4,15] = 1
-## c
-a[2::4,5] = -1
-a[2::4,15] = 1
-## d
-a[3::4,5] = 1
-a[3::4,15] = -1
+class MyDataGenerator(Dataset):
+    """
+    testing my first pytorch-Dataset implementing an
+        data set generator at start-up
+    """
+    def __init__(self,train=True):
+        """
+        function where intial logic happens like reading, assiging parameter, generating a set of data
+        """
+        if train == True: 
+            n   = 1000
+        else:
+            n = 200
+        t   = 20
+        a   = np.zeros((n,t),dtype=np.float32) 
+        ## a
+        a[::4,5] = -1
+        a[::4,15] = -1
+        ## b
+        a[1::4,5] = 1
+        a[1::4,15] = 1
+        ## c
+        a[2::4,5] = -1
+        a[2::4,15] = 1
+        ## d
+        a[3::4,5] = 1
+        a[3::4,15] = -1
 
-a   += np.random.randn(n,t)*.3
-b   = np.zeros((n), dtype=np.float32)
-b[1::4] = 1
-b[2::4] = 2
-b[3::4] = 3
+        a   += np.random.randn(n,t)*.3
+        b   = np.zeros((n), dtype=np.float32)
+        b[1::4] = 1
+        b[2::4] = 2
+        b[3::4] = 3
+        self.data   = a
+        self.target = b
+        """
+        x_train = a[:800,:]
+        y_train = b[:800]
 
+        x_test  = a[800:,:]
+        y_test  = a[800:]
+        """
+    def __getitem__(self,index):
+        """
+        retrun a tuple of data set and label of data set
+            index is one or more realizations?
+        """
+        return (self.data[index,:],self.target[index])
 
-x_train = a[:800,:]
-y_train = b[:800]
+    def __len__(self):
+        """
+        functions returns count of given realizations
+        """
+        return self.target.shape[0]
 
-x_test  = a[800:,:]
-y_test  = a[800:]
-
+#dataset = MyDataGenerator
+train_loader = DataLoader(MyDataGenerator(train=True),shuffle=True,batch_size=800)
+test_loader = DataLoader(MyDataGenerator(train=False),batch_size=200)
 
 #plt.plot(a[:10,:].T);plt.show()
 # Linear Regression Model
@@ -69,7 +104,7 @@ class DenseLayers(nn.Module):
         h   = self.relu(self.batch_n(self.linear1(x)))
         out   = self.linear2(h)
         return out
-
+t = 20
 input_size	= t
 output_size	= 1
 hidden1_size    = 100
@@ -85,26 +120,23 @@ rnd             = np.arange(0,800)
 
 for i in range(10):
     print i
-    for epoch in range(500):
-        rnd = np.random.permutation(rnd)
-        inputs = Variable(torch.from_numpy(x_train[rnd,:]))
-        targets = Variable(torch.from_numpy(y_train[rnd]))
-
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        print epoch,':',loss.data[0]
-
-    predicted = model(Variable(torch.from_numpy(x_test))).data.numpy()
-    plt.hist(predicted[::4])
-    plt.hist(predicted[1::4])
-    plt.hist(predicted[2::4])
-    plt.hist(predicted[3::4])
-    plt.show()
-#print predicted
-#plt.plot(a, y_train, 'ro', label='Original data')
-#plt.plot(a, predicted, label='Fitted line')
-#plt.legend()
-#plt.show()
+	##model.train()
+    for epoch in range(100):
+		for (inputs, targets) in train_loader:
+				inputs = Variable(inputs)
+				targets = Variable(targets)
+				optimizer.zero_grad()
+				outputs = model(inputs)
+				loss = criterion(outputs, targets)
+				loss.backward()
+				optimizer.step()
+        
+		print epoch,':',loss.data[0]
+	##model.eval()?
+    for test_data,targets in test_loader:
+			predicted = model(Variable(test_data)).data.numpy()
+			plt.hist(predicted[::4])
+			plt.hist(predicted[1::4])
+			plt.hist(predicted[2::4])
+			plt.hist(predicted[3::4])
+			plt.show()
